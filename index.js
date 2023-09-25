987bad53c14203fd4c6f934b3896d8e96b0d7010
Антон Фондов <antonren@yandex.ru>
2023-09-25 14:23:34 +0700

    Сделал отправку запроса в 1с с Авторизацией

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-const express = require('express');
-const { PORT = 3207 } = process.env;
-const app = express();
+const express = require('express')
+const {PORT = 3207} = process.env
+const app = express()
+const axios = require('axios').default
 
+const TelegramBot = require('node-telegram-bot-api')
+const token = '6267511241:AAECCtUtxlBbkbXExvREMKUvFXyFp4iNY14'
+// const chatId = '996600001'
+const bot = new TelegramBot(token, {polling: true})
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = '6131297068:AAHk1AbCRpq5gm4kkYiKKp__vfREeDOZASY';
-const chatId = '996600001'
-const bot = new TelegramBot(token, {polling: true});
-
-
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
-
+const auth = Buffer.from('Администратор:2609').toString('base64')
+const url = 'http://localhost/CopyUT/hs/mobile/getUser'
+const bodyRequest = { name: 'Фондов'}
+getUserAx()
 
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+    const chatId = msg.chat.id
+    const data = handleCommands(msg.text)
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, msg.text);
-});
-
-app.get('/', (req, res) => {
-    res.send(
-        `<html>
-        <body>
-            <h1>Ответ на сигнал из далёкого космоса</h1>
-        </body>
-        </html>`
-    );
-    const ip = getRequestIpAddress(req)
-    bot.sendMessage(chatId, 'Get порта 3207, ip: ' + ip)
-    console.log(chatId, 'Get порта 3207, ip: ' + ip)
+    if (data !== '') {
+        getUser()
+        bot.sendMessage(chatId, data)
+    }
 })
 
-const getRequestIpAddress = request => {
-
-    const client = request.connection ?? request.socket ?? request.info;
-    if (client) {
-        return client.remoteAddress ?? null;
+async function getUser() {
+    try {
+        const response = await axios({
+            url,
+            method: 'GET',
+            auth: {
+                username: 'Администратор',
+                password: '2609'
+            },
+            data: JSON.stringify(bodyRequest),
+            responseType: 'json'
+        })
+        return response.data
+    } catch (error) {
+        return ''
     }
-    return null;
+
+
+}
+
+
+function handleCommands(commands) {
+
+
+    if (commands === '/getUser') {
+
+
+        return 'it`s Working'
+    }
+
+    return ''
 }
