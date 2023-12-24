46d85b4ee2c4e3440f5fc4abbe3760ba26c15c1c
antonfondov <antonren@yandex.ru>
2023-12-24 14:22:12 +0700

    Вернул бота

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const api = require('./Api')
 const { json } = require('express');
 
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5736645804:AAFo3eb8VrSJYdD2ISvPQlw-CFrE3aUWlKI';
+const chatId = '996600001'
+const bot = new TelegramBot(token, {polling: true});
+
+
 app.listen(PORT, async () => {
     console.log(`App listening on port ${PORT}`)
 })
@@ -16,12 +22,20 @@ app.get('/', async (req, res) => {
         </body>
         </html>`
     );
-    const ip = getRequestIpAddress(req)
-    console.log('Get порта 3207, ip: ' + ip)
+
     const users = await api.getUser()
     console.log(users);
+    const ip = getRequestIpAddress(req)
+    bot.sendMessage(chatId, 'Get порта 3207, ip: ' + ip)
+    console.log(chatId, 'Get порта 3207, ip: ' + ip)
 })
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, msg.text);
+});
 const getRequestIpAddress = request => {
 
     const client = request.connection ?? request.socket ?? request.info;
